ac0b9421f594b5c4827e27941d39a2b819e6c08f
SharmaAkshay42 <47718946+SharmaAkshay42@users.noreply.github.com>
2022-11-01 02:43:20 -0700

    Updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/simple-todo-first/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   },
 });
